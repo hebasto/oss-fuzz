136869127a436f0ff62e54ebd05c045cb5750b69
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-09-22 21:24:00 +0100

    A fix

--- projects/bitcoin-core/build.sh
+++ projects/bitcoin-core/build.sh
@@ -38,7 +38,7 @@ fi
 # export LDFLAGS="-flto=thin"
 
 # Use lld to workaround <module> referenced in <section> of /tmp/lto-llvm-*.o: defined in discarded section
-export LDFLAGS="-fuse-ld=lld"
+export LDFLAGS="-fuse-ld=lld -L/usr/local/lib/clang/22/lib/i386-unknown-linux-gnu"
 
 export CPPFLAGS="-D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_DEBUG -DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE"
 
